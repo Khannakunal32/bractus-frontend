c771389a929d2ee32099933722c9c84595be9884
Bractusofficial <bractusofficial@gmail.com>
2021-06-26 20:58:07 +0530

    sonal bulletin offer add

--- src/screens/leaderBoard.js
+++ src/screens/leaderBoard.js
@@ -17,14 +17,14 @@ export default function leaderBoard() {
             maxWidth: "350px",
             display: "flex",
             flexDirection: "column",
-            alignItems: "center",
+            alignItems: "left",
             margin: "10px",
             justifyContent: "center",
             border: "1px solid grey",
-            padding: "10px",
+            padding: "20px",
           }}
         >
-          <h3 >Give Away!!!</h3>
+          <h3>Give Away!!!</h3>
           <div className="price">Rs. 2800</div>
           <h2>Requirements:</h2>
           <li>2 degree package and above</li>
@@ -33,18 +33,20 @@ export default function leaderBoard() {
           <li>Actively working in Bractus</li>
           <p style={{ textAlign: "left" }}>
             {" "}
-            Two Prospects are availaible with billing done and their mentors are not yet paid
-            they need a new mentor . Their mentor will be chosen as above and are needed to guide
-            them about marketing. Mentor will be provided with 1400 each to teach them and train them
-            in their team.
+            Two Prospects are availaible with billing done and their mentors are
+            not yet paid they need a new mentor . Their mentor will be chosen as
+            above and are needed to guide them about marketing. Mentor will be
+            provided with 1400 each to teach them and train them in their team.
           </p>
           <p style={{ textAlign: "left" }}>
             {" "}
-            Mentor need to train them for atleast 1 week properly and if the prospects are comfortable and 
-            satisfied with you . You will recieve your bonus. 500 will be given in advance. Else you need to 
-            refund the whole mentorship price given to you. DM on group i want the offer to avail this.
+            Mentor need to train them for atleast 1 week properly and if the
+            prospects are comfortable and satisfied with you . You will recieve
+            your bonus. 500 will be given in advance. Else you need to refund
+            the whole mentorship price given to you. DM on group i want the
+            offer to avail this.
           </p>
-          <h1>Note:Terms and Conditions applied.</h1>
+          <h1>Note: Terms and Conditions applied.</h1>
         </div>
 
         <div
@@ -52,14 +54,14 @@ export default function leaderBoard() {
             maxWidth: "350px",
             display: "flex",
             flexDirection: "column",
-            alignItems: "center",
+            alignItems: "left",
             margin: "10px",
             justifyContent: "center",
             border: "1px solid grey",
-            padding: "10px",
+            padding: "20px",
           }}
         >
-          <h3>OFFER 2</h3>
+          {/* <h3>OFFER 2</h3>
           <div className="price">Soon will be available</div>
           <h2>Descriptions/Requirements:</h2>
           <li>Requirements 1</li>
@@ -76,6 +78,35 @@ export default function leaderBoard() {
             resourses you are getting from Bractus.
           </p>
           <h1>Note:All terms and conditions applied</h1>
+        </div>
+      </div> */}
+          <h3>
+            Get your own teaser video easily.<br></br> Grow/start your earning
+            with this.
+          </h3>
+          <h2>By Sonal Bongade - </h2>
+          <p style={{ textAlign: "left" }}>
+            Sonal Bongade ~ "Anyone who wants to create their teaser videos or
+            want to make posters or want to edit any images can contact me . I
+            will provide you customized videos and photos or posters regarding
+            any topic which you want (charges will be applied ) .I assure you
+            that your work will be done in given period of time."<br></br>Contact
+            details - 8855049400
+          </p>
+          <h2>Description</h2>
+          <li>
+            <a href=" https://youtu.be/51zsrRwrls0">Demo video/work</a>
+          </li>
+          <li>Wanna have video prepared for free?? </li>
+          <p>To make a deal. Read below...</p>
+          <p style={{ textAlign: "left" }}>
+            {" "}
+            Let her make a video for you in advance and after you get a payout
+            from Bractus. A small amount after both of your consent will be
+            given to her directly from yours. After making a deal mail your
+            decision to bractus.
+          </p>
+          <h1>Note: The teaser video is crucial part for your business.</h1>
         </div>
       </div>
     </div>
