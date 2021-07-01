c71a460704c90227a09c0f193b6e3fa09b227dda
Bractusofficial <bractusofficial@gmail.com>
2021-07-01 18:11:47 +0530

    mentorship 8 videos

--- src/screens/kunalMentorship.js
+++ src/screens/kunalMentorship.js
@@ -126,7 +126,7 @@ export default function kunalMentorship() {
               <img
                 width="400"
                 height="200"
-                src={zoom}
+                src="https://www.certusrecruitment.com/rails/active_storage/representations/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBMHcvQ0E9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--a69b98ed59d466b75065eb04dfccc3911db460fb/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCam9VWTI5dFltbHVaVjl2Y0hScGIyNXpld2c2QzNKbGMybDZaVWtpRGpFd01EQjROVEF3WGdZNkJrVlVPZ3huY21GMmFYUjVTU0lMUTJWdWRHVnlCanNIVkRvSlkzSnZjRWtpRVRFd01EQjROVEF3S3pBck1BWTdCMVE9IiwiZXhwIjpudWxsLCJwdXIiOiJ2YXJpYXRpb24ifX0=--46c71b34483be69bdb6c526f3ee4da2b74b4cce2/Growth%20vs%20Fixed%20(2).png"
                 alt="Mindset Part 1"
               />
             </a>
@@ -137,8 +137,8 @@ export default function kunalMentorship() {
             <a href="https://youtu.be/KBoDrqFB7GY" title="Mindset Part 2">
               <img
                 width="400"
-                height="200"
-                src={zoom}
+                height="250"
+                src="https://static.vecteezy.com/system/resources/previews/001/225/333/non_2x/growth-mindset-and-fixed-mindset-concept-vector.jpg"
                 alt="Mindset Part 2"
               />
             </a>
@@ -188,7 +188,7 @@ export default function kunalMentorship() {
               <img
                 width="400"
                 height="200"
-                src={zoom}
+                src="https://www.humanresourcesblog.in/wp-content/uploads/2011/10/How-Social-media-impacts-hiring.png"
                 alt="Skillset - Profiling"
               />
             </a>
@@ -289,7 +289,7 @@ export default function kunalMentorship() {
               <img
                 width="400"
                 height="200"
-                src={zoom}
+                src="https://www.bizprospex.com/wp-content/uploads/2018/10/coldd.jpg"
                 alt="Skillset - Invitation Hot-Warm"
               />
             </a>
@@ -355,7 +355,7 @@ export default function kunalMentorship() {
         <ol>
           {" "}
           <li>
-            Watch Skillset - Invitation Hot-Warm <br></br>
+            Watch Pre-Immuning Hot-Warm <br></br>
             <a
               href="https://youtu.be/O1PSVrOT6Do"
               title="Skillset Pre-Immuning Hot-Warm"
@@ -363,7 +363,7 @@ export default function kunalMentorship() {
               <img
                 width="400"
                 height="200"
-                src={zoom}
+                src="https://media.sproutsocial.com/uploads/2018/05/Customers_Part_of_Company_Blog.png"
                 alt="Skillset - Pre-Immuning Hot-Warm"
               />
             </a>
@@ -463,7 +463,7 @@ export default function kunalMentorship() {
 
       <hr></hr>
 
-{/* <div
+<div
   style={{
     display: "flex",
     flexDirection: "column",
@@ -478,48 +478,39 @@ export default function kunalMentorship() {
     style={{
       textAlign: "center",
     }}
-  > */}
-    <h1>Day 6</h1>{" "}
-  {/* </div>
+  >
+    <h1>Day 6</h1>
+  </div>
   <p>Task for the day 6 is following:</p>
   <ol>
     <li>
-      Watch Skillset - Relation Building 1 <br></br>
+      Watch Skillset - Lead Generation  <br></br>
       <a
-        href="https://youtu.be/yWLilnwar-I"
-        title="Skillset - Relation Building 1"
+        href="https://youtu.be/ZF06UQngDos"
+        title="Skillset 6 Lead Generation -1 "
       >
         <img
           width="400"
           height="200"
-          src="https://i9.ytimg.com/vi_webp/cM_5GI1hyac/mqdefault.webp?v=607ffdd4&sqp=CPSm7IYG&rs=AOn4CLCj9pLTYg9j8rtj8J8iArqw3OUyLw"
-          alt="Skillset - Relation Building 1"
+          src="https://www.ninjarmm.com/wp-content/uploads/2020/08/msp-remote-lead-generation-ideas-.jpg"
+          alt="Skillset - Lead Generation 1"
         />
       </a>
     </li>
+<li>Finalize your profiling work.</li>
+    <li>Start daily lead generation. </li>
     <li>
-      Listen to below audio now. <br></br>
-      <iframe
-        frameborder="0"
-        width="500"
-        height="100"
-        title="kal"
-        src="https://drive.google.com/file/d/1K5KQpY6raMKfrszas5OCK3JOAe3GTJ0y/preview?usp=sharing"
-      ></iframe>
-    </li>
-    <li>Finalize your profiling as best as possible.</li>
-    <li>
-      Use hot warm list 5 people more.
+    But keep focus on hot-warm as well.
     </li>
     <li>
-    Make your instagram profile public.
+    Listen to cold call recordings (soon available).
     </li>
-  
+      <li>Pdf of chats will be attached here soon.</li>
   </ol>
 </div>
 <h1>
   Note: Please make sure to watch all video and complete the task to earn.
-</h1> */}
+</h1>
 
     <hr></hr>
 
@@ -552,7 +543,8 @@ export default function kunalMentorship() {
               <img
                 width="400"
                 height="200"
-                src={zoom}
+           
+                src="https://www.hashmicro.com/blog/wp-content/uploads/2016/02/5-1.jpg"
                 alt="Skillset - Relation Building 1"
               />
             </a>
@@ -567,13 +559,12 @@ export default function kunalMentorship() {
               src="https://drive.google.com/file/d/1K5KQpY6raMKfrszas5OCK3JOAe3GTJ0y/preview?usp=sharing"
             ></iframe>
           </li>
-          <li>Finalize your profiling as best as possible.</li>
+          <li>Practice callings with your mentor.</li>
+          <li>Spend time with your mentor asking their calling experience.</li>
           <li>
-            Use hot warm list 5 people more.
+            Attack hot warm list people Callings.
           </li>
-          <li>
-          Make your instagram profile public.
-          </li>
+          
         
         </ol>
       </div>
@@ -612,23 +603,12 @@ export default function kunalMentorship() {
               <img
                 width="400"
                 height="200"
-                src={zoom}
+                 src="https://i1.wp.com/growthlynx.com/wp-content/uploads/2017/01/cropped-peopel-in-a-business-meeting-shaking-hands-computer-on-table11.jpg?ssl=1"
                 alt="Skillset - Relation Building 2"
               />
             </a>
           </li>
-         
-          <li>Start pre-immuning your friends whom you've called before.</li>
-          <li>
-            Call more hot-warm friends and start scheduling the buiness webinars
-            for them.
-          </li>
-          <li>
-           
-            Call 5 people from hot warm market and take help of your Mentor.
-          </li>
-      
-        </ol>
+      </ol>
       </div>
       <h1>
         Note: Please make sure to watch all video and complete the task to earn.
